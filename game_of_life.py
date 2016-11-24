b1cac41993312a7ee8488f2e79877968b9c7374b
Piotr Jedrys <Piotr.Jedrys@tomtom.com>
2016-11-24 09:49:19 +0100

    step 4: add tick

--- game_of_life.py
+++ game_of_life.py
@@ -3,6 +3,9 @@ class GameOfLife(object):
     def __init__(self, seed):
         self.alive_cells = seed
 
+    def tick(self):
+        return self.alive_cells
+
 def generator(seed):
     yield set([])
 
--- game_of_life_tests.py
+++ game_of_life_tests.py
@@ -11,6 +11,15 @@ class TestGameOfLife(unittest.TestCase):
         # THEN
         self.assertEqual(game_of_life.alive_cells, seed)
     
+    def test_game_of_life_empty_tick(self):
+        # GIVEN
+        seed = set()
+        game_of_life = GameOfLife(seed)
+        # WHEN
+        next_generation = game_of_life.tick()
+        # THEN
+        self.assertEqual(next_generation, seed)
+
     def test_empty_seed_generation(self):
         """
         ...
