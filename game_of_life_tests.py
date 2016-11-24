c2ac68a585ba254869c02b21d0ed4e6bd4a550b4
Piotr Jedrys <Piotr.Jedrys@tomtom.com>
2016-11-24 09:11:49 +0100

    2 step: objectize

---
+++ game_of_life_tests.py
@@ -0,0 +1,35 @@
+import unittest
+from game_of_life import GameOfLife, generator
+
+class TestGameOfLife(unittest.TestCase):
+
+    def test_game_of_life_object_initialization(self):
+        # GIVEN
+        seed = set([])
+        # WHEN
+        game_of_life = GameOfLife(seed)
+        # THEN
+        self.assertEqual(game_of_life.alive_cells, seed)
+    
+    def test_empty_seed_generation(self):
+        """
+        ...
+        ...
+        ...
+        
+        =>
+        
+        ...
+        ...
+        ...
+        """
+        # GIVEN
+        seed = set([])
+        expected = set([])
+        # WHEN
+        next_generation = generator(seed)
+        # THEN
+        self.assertEqual(next_generation, expected)
+
+if __name__ == "__main__":
+    unittest.main()
