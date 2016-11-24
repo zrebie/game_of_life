62b868dec2101a186b00208c3877b290642d4456
Piotr Jedrys <Piotr.Jedrys@tomtom.com>
2016-11-24 11:33:57 +0100

    step 6: neighbours

--- game_of_life.py
+++ game_of_life.py
@@ -1,10 +1,22 @@
 class GameOfLife(object):
-    
+
     def __init__(self, seed):
         self.alive_cells = seed
 
     def tick(self):
+        self.alive_cells = set()
         return self.alive_cells
+
+    @staticmethod
+    def get_neighbours(cell):
+        neighbours = set()
+        deltas = set([(-1, -1), (0, -1), (1, -1),
+                      (-1,  0),          (1,  0),
+                      (-1,  1), (0,  1), (1,  1)])
+        for dx, dy in deltas:
+            neighbours_cell = (cell[0]+dx, cell[1]+dy)
+            neighbours.add(neighbours_cell)
+        return neighbours
 
 def generator(seed):
     game_of_life = GameOfLife(seed)
--- game_of_life_tests.py
+++ game_of_life_tests.py
@@ -39,6 +39,58 @@ class TestGameOfLife(unittest.TestCase):
         next_generation = next(generator(seed))
         # THEN
         self.assertEqual(next_generation, expected)
+# 
+#     def test_tick_block_should_survive(self):
+#         """
+#         **
+#         **
+#         =>
+#         **
+#         **
+#         """
+#         # GIVEN
+#         seed = set([(0,0), (0,1),(1,0),(1,1)])
+#         expected = seed
+#         game_of_life = GameOfLife(seed)
+#         # WHEN & THEN
+#         self.assertEqual(game_of_life.tick(), expected)
+    
+    def test_should_return_correct_neighbours(self):
+        # GIVEN
+        cell = (0,0)
+        expected_len = 8
+        expected_neighbours = set([(-1,-1), (0,-1), (1, -1),
+                                   (-1,0),     (1, 0),
+                                   (-1,1), (0,1), (1, 1)])
+        # WHEN
+        neighbours = GameOfLife.get_neighbours(cell)
+        # THEN
+        self.assertEqual(expected_len, len(neighbours))
+        self.assertEqual(expected_neighbours, neighbours)
+
+    def test_should_return_correct_neighbours_any_cell(self):
+        # GIVEN
+        cell = (14,3)
+        expected_len = 8
+        expected_neighbours = set([(13,2), (14,2), (15, 2),
+                                   (13,3),     (15, 3),
+                                   (13,4), (14,4), (15, 4)])
+        # WHEN
+        neighbours = GameOfLife.get_neighbours(cell)
+        # THEN
+        self.assertEqual(expected_len, len(neighbours))
+        self.assertEqual(expected_neighbours, neighbours)
+
+    def test_tick_with_one_cell_dies(self):
+        # GIVEN
+        seed = set([(0,0)])
+        expected_1 = set()
+        expected_2 = set()
+        game_of_life = GameOfLife(seed)
+        # WHEN & THEN
+        self.assertEqual(game_of_life.tick(), expected_1)
+        self.assertEqual(game_of_life.tick(), expected_2)
+
 
 if __name__ == "__main__":
     unittest.main()
